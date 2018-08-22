ce02436e807758b22ada770ee8706eb0d3212f6c
gv4383 <gregory.vargas18@gmail.com>
2018-08-22 16:13:16 -0500

    Updates

---
+++ path---index-4c14a2e936e1b5fd637c.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{420:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Greg Vargas",description:"A portfolio to showcase my journey in software development"}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-4c14a2e936e1b5fd637c.js.map
